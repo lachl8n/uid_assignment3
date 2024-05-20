eca86474a2c5129615e7065fd3bcd6d877044a33
lkchue <lachlan.chue@student.unimelb.edu.au>
2024-05-20 16:49:25 +1000

    started javascript file, created show and hide functionality of product dropdown and search bar

---
+++ script.js
@@ -0,0 +1,16 @@
+function showSearch() {
+    document.getElementById("search-section").style.opacity = "100%";
+    document.getElementById("page-container").style.filter = "blur(8px)";
+}
+
+function closeSearch() {
+    document.getElementById("search-section").style.opacity = "0";
+    document.getElementById("page-container").style.filter = "none";
+}
+
+function showProductDropdown() {
+    document.getElementById("product-drop-down").style.opacity = "100%";
+    document.addEventListener('mouseout', ()=> {
+        document.getElementById("product-drop-down").style.opacity = "0";
+    })
+}
