6e749ee75d98dfc3337c2879110c872334796a87
lkchue <lachlan.chue@student.unimelb.edu.au>
2024-05-24 21:49:24 +1000

    created functionality to show and hide product information

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+let backgroundBlurred = false;
+
 function showSearch() {
     document.getElementById("search-section").style.opacity = "100%";
-    document.getElementById("page-container").style.filter = "blur(8px)";
     document.getElementById("search-input").focus();
+    blurBackground();
 }
 
 function closeSearch() {
     document.getElementById("search-section").style.opacity = "0";
-    document.getElementById("page-container").style.filter = "none";
+    document.getElementById("page-content").style.filter = "none";
+    document.getElementById("page-content").style.pointerEvents = "all";
 }
 
 function showProductDropdown() {
@@ -14,4 +17,33 @@ function showProductDropdown() {
     document.getElementById("product-drop-down").addEventListener("mouseleave", ()=> {
         document.getElementById("product-drop-down").style.opacity = "0";
     })
+}
+
+function showProduct() {
+    document.getElementById("body").style.overflow = "hidden";
+    document.getElementById("product-banana").style.opacity = "100%";
+    blurBackground();
+}
+
+function blurBackground() {
+    console.log(backgroundBlurred);
+    document.getElementById("page-content").style.filter = "blur(5px)";
+    document.getElementById("page-content").style.pointerEvents = "none";
+    backgroundBlurred = true;
+    console.log("BRUH");
+}
+
+function unblurBackground() {
+    if (backgroundBlurred) {
+        document.getElementById("page-container").addEventListener("click", () => {
+            console.log("leave");
+            document.getElementById("page-content").style.filter = "none";
+            document.getElementById("page-content").style.pointerEvents = "all";
+            document.getElementById("body").style.overflow = "none";
+            document.getElementById("product-banana").style.opacity = "0";
+            location.reload();
+        })
+    }
+    
+    backgroundBlurred = false;
 }
