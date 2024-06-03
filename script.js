d7fbc58325e1b661cd442b857967340f014f788e
nnxcd <nadiaalexand@student.unimelb.edu.au>
2024-06-03 17:33:40 +1000

    Implemented initial responsive design

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function showProduct(id) {
         productImage = "assets/images/products/bananas.jpeg";
         productName = "Bananas";
         productPrice = "$6.00"
-        productRating = "4,9";
+        productRating = "4.9";
         productOrders = "130";
         productWeight = "180";
         productDescription = "Go bananas! These banana flavoured sweets are sure to satisfy little monkeys and big monkeys alike!";
@@ -69,7 +69,7 @@ function showProduct(id) {
         productImage = "assets/images/products/conversation-hearts.png";
         productName = "Conversation Hearts";
         productPrice = "$6.50"
-        productRating = "4,6";
+        productRating = "4.6";
         productOrders = "80";
         productWeight = "180";
         productDescription = "Let your sweet heart know you love them by giving them one of these sweet compressed candy love hearts with sweet messages on them.";
@@ -78,8 +78,8 @@ function showProduct(id) {
         productImage = "assets/images/products/blue-raspberry-wheels.jpg";
         productName = "Blue Raspberry Wheels";
         productPrice = "$6.50"
-        productRating = "0";
-        productOrders = "0";
+        productRating = "3.2";
+        productOrders = "100";
         productWeight = "160";
         productDescription = "Raspberry flavoured licorice wheels.";
         productIngredients = "Ingredients: Sugar, Glucose Syrup, Modified Starch (Wheat flour starch), Stabilisers( Sorbitol syrup, Glycerol), Acids (Citric Acid, Malic Acid, Lactic Acid, Fumeric Acid), Dextrose, Coconut Oil, Gelatine, Flavourings, Emusifier (Mono and Diglycerides of fatty acids), Acidity Regulator (Trisodium Citrate), Glazing Agent (Beeswax), Colour (Brilliant Blue FCF).";
@@ -88,8 +88,8 @@ function showProduct(id) {
         productImage = "assets/images/products/candy-bullets.jpeg";
         productName = "Candy Bullets";
         productPrice = "$6.00"
-        productRating = "0";
-        productOrders = "0";
+        productRating = "4.0";
+        productOrders = "30";
         productWeight = "200";
         productDescription = "Brightly coloured candy coated licorice. A bright bite!";
         productIngredients = "Ingredients: Glucose Syrup (Corn, Wheat - contains Sulphites), Sugar, Wheat Starch, Colours (123, 133), Flavours, Glazing Agents (901, 904, Orange Oil)";
@@ -97,8 +97,8 @@ function showProduct(id) {
         productImage ="assets/images/products/cherry-wheels.jpeg";
         productName = "Cherry Wheels";
         productPrice = "$6.50"
-        productRating = "0";
-        productOrders = "0";
+        productRating = "4.7";
+        productOrders = "150";
         productWeight = "160";
         productDescription = "Cherry flavoured licorice wheels.";
         productIngredients = "Ingredients: Glucose Syrup, Sugar, Corn Starch, Wheat Flour, Stabilisers (Glycerol, Sorbitol), Emulsifier (E471), Acid (Citric), Flavourings, Colour (E120), Acidity Regulator (Sodium Citrate), Vegetable Oil, Glazing Agent (Bees Wax)";
@@ -107,8 +107,8 @@ function showProduct(id) {
         productImage = "assets/images/products/almond-brittle.jpeg";
         productName = "Almond Brittle";
         productPrice = "$7.00"
-        productRating = "0";
-        productOrders = "0";
+        productRating = "3.8";
+        productOrders = "90";
         productWeight = "190";
         productDescription = "Almonds combine with a light caramel to create a brittle to rival the original.";
         productIngredients = "Ingredients: Sugar, Almonds, Glucose (Corn), Honey, Butter, Bicarb Soda.";
@@ -117,8 +117,8 @@ function showProduct(id) {
         productImage = "assets/images/products/dolly-mix.jpeg";
         productName = "Dolly Mix";
         productPrice = "$6.00"
-        productRating = "0";
-        productOrders = "0";
+        productRating = "3.9";
+        productOrders = "40";
         productWeight = "185";
         productDescription = "Dolly mix is a beautiful mix of small fruit flavoured coconut, foam and jelly sweets.";
         productIngredients = "Ingredients: Sugar, Glucose Syrup, Vegetable Oil, Beef Gelatine, Cornflour, Colours (Anthocyanins, Paprika Extract, Curcumin, Chlorophylls, Beetroot Red), Flavourings, Acid (Citric Acid), Fat Reduced Cocoa Powder, Fruit and Vegetable Concentrates (Apple, Hibicus), Glazing Agent (Pectin)";
@@ -127,8 +127,8 @@ function showProduct(id) {
         productImage = "assets/images/products/fizzoes.jpeg";
         productName = "Fizzoes";
         productPrice = "$5.50"
-        productRating = "0";
-        productOrders = "0";
+        productRating = "4.3";
+        productOrders = "20";
         productWeight = "200";
         productDescription = "Fizzoes are full of fizzy fun! A hard round sweet, with a fizzy sherbet coating.";
         productIngredients = "Ingredients: Sugar, Glucose, Flavour, Colours (102, 110, 122, 123, 124, 133, 142), Food Acids (330, 334), Sodium Bicarbonate (500)";
